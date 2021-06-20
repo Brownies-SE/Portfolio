d60d2d4e77e5bbd89b79faed24e3e2a07f5e6c1d
Josh Brown <brownjoshalan@gmail.com>
2021-06-20 17:58:00 -0600

    Created contact page, simple text for now

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1>This is going to be my Contact page</h1>
+      <p>This is my contact information</p>
+    </div>
+  );
+}
+
+export default Contact;
