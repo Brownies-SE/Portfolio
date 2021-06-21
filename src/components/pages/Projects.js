3e217589c7058b9a04dc27a634b8d3ceb50d1a89
Josh Brown <brownjoshalan@gmail.com>
2021-06-20 18:00:02 -0600

    Created my projects page, simple text for now

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div>
+      <h1>This is going to be my project page</h1>
+      <p>This is my current projects</p>
+    </div>
+  );
+}
+
+export default Projects;
